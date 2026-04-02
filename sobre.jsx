00f3d689cf44b48432166d89d81b13d38839491a
jelensousa07-dev <juanaelen13@gmail.com>
2026-04-02 12:31:12 -0300

    Delete sobre.jsx

--- sobre.jsx
+++
@@ -1,9 +0,0 @@
-const Sobre = () => {
-  return (
-    <section id="sobre" style={{padding: '40px', textAlign: 'center'}}>
-      <h2>Sobre Mim</h2>
-      <p>Estudante de Sistemas e Contabilidade e Social Media .</p>
-    </section>
-  );
-};
-export default Sobre;
